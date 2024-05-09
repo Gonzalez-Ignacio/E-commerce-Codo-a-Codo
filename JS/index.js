bd5716860de53e66cdadb36f76555bce352c0d90
Facundo Blanco <facuu0505@gmail.com>
2024-05-09 19:42:13 -0300

    primer commit de productos

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+
+/*
+function realizarBusqueda() {
+    var busqueda = document.getElementById('campo-busqueda').value;
+    // Aquí puedes hacer lo que quieras con el valor de la búsqueda, como enviarlo a un servidor o realizar alguna acción en JavaScript.
+    console.log("Búsqueda realizada:", busqueda);
+  }
+  */
