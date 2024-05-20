d7fe89a0424d846537bb2765faf419bf2293769e
Diego Zuccolilli <Dzuccolilli@gmail.com>
2024-05-19 23:25:17 -0300

    algunas medias querys

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,24 @@
 
-/*
-function realizarBusqueda() {
-    var busqueda = document.getElementById('campo-busqueda').value;
-    // Aquí puedes hacer lo que quieras con el valor de la búsqueda, como enviarlo a un servidor o realizar alguna acción en JavaScript.
-    console.log("Búsqueda realizada:", busqueda);
+const botonBusqueda = document.getElementById("button-search");
+const sugerencias =["velas", "home-spray", "difusores"];
+
+
+
+function mostrarBusqueda(event) {
+  event.preventDefault();
+  let inputSearch = document.getElementById("buscar").value.toLowerCase(); // Convertir el valor de entrada a minúsculas
+
+  if (inputSearch === "") {
+    alert("Debe ingresar un valor");
+  } else if (inputSearch == "velas") { // Utilizar "else if" para evitar evaluaciones innecesarias
+    window.location.href = "/productos/velas/velas.html";
+  } else if (inputSearch == "homespray") {
+    window.location.href = "/productos/home-spray/home-spray.html";
+  } else if (inputSearch == "difusores") {
+    window.location.href = "/productos/difusores/difusores.html";
+  } else {
+    alert("No se encontraron resultados para la búsqueda: " + inputSearch); // Agregar una alerta para manejar entradas no válidas
   }
-  */
+}
+
+botonBusqueda.addEventListener("click", mostrarBusqueda);
